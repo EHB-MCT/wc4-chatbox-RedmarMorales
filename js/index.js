cf3f4b4a8b5065e010bc2c69828dc23e5fdf7e5c
Redmar Morales <redmarfrancis@yahoo.com>
2022-03-17 09:37:28 +0100

    code aangepast

--- js/index.js
+++ js/index.js
@@ -1,16 +1,56 @@
 "use strict";
 
-// url =>  https://ehbchatapp.herokuapp.com/
+// url =>  https://ehbchatapp.herokuapp.com
 
 const chat = {
-    author: "yourName",
+    author: "Francis",
     init() {
+        this.fetchMessages();
+        this.sendMessage();
     },
     sendMessage() {
+        //fetch()
+        let msg = "";
+
+        fetch('https://ehbchatapp.herokuapp.com/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                author: this.author,
+                message: 'Hello!'
+            })
+        });
     },
     fetchMessages() {
+        console.log(this.fetchMessages);
+        fetch('https://ehbchatapp.herokuapp.com/messages')
+            .then(response => {
+                return response.json();
+
+            })
+            .then(data => {
+                data.forEach(message => {
+                    this.renderMessage(message);
+                });
+            })
     },
     renderMessage(message) {
+        console.log("Render!", message);
+        const containerHtml = document.getElementById('messageContainer');
+
+        const htmlString = `<div class="messageItem">
+          <div class="header">
+              <span class="author">${message.author}</span>
+              <span class="time">00:00</span>
+          </div>
+          <p>
+              ${message.message}
+          </p>
+      </div>`
+
+        containerHtml.innerHTML += htmlString;
     }
 
 }
